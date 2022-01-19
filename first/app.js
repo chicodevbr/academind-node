77a206a5bf92bfe2a032086fe4742232a4bf09a8
chicodevbr <chicodevbr@gmail.com>
2022-01-19 10:21:18 -0300

    serving html pages

--- first/app.js
+++ first/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const adminRoutes = require('./routes/admin');
@@ -16,7 +17,7 @@ app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
 app.use('/', (req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
