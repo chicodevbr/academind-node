fcd7c62f9e43dcc1845f53598735224e0aa1f6d1
chicodevbr <chicodevbr@gmail.com>
2022-01-31 17:16:44 -0300

    layout pug

--- first/app.js
+++ first/app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-const app = express();
 
 app.use(express.json());
 app.use(
