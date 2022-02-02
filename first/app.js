6e9c87b9b01b9757e79e1bae82699be08533b0e8
chicodevbr <chicodevbr@gmail.com>
2022-02-02 09:27:49 -0300

    add controllers folder

--- first/app.js
+++ first/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.json());
@@ -18,7 +18,7 @@ app.use(
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoutes);
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 
 app.use('/', (req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' });
