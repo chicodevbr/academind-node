de543580bee32961da2ec5eb174a2afc41680237
chicodevbr <chicodevbr@gmail.com>
2022-02-02 23:00:55 -0300

    finishing controllers

--- first/app.js
+++ first/app.js
@@ -7,6 +7,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(express.json());
 app.use(
@@ -20,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
-app.use('/', (req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
