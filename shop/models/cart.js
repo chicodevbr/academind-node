b608cc3f41974853e84f0fb86d2b15681794af66
chicodevbr <chicodevbr@gmail.com>
2022-02-05 10:42:18 -0300

    add editing page

--- shop/models/cart.js
+++ shop/models/cart.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
     // Fetch the previous cart
-    fs.readFileSync(p, (err, fileContent) => {
+    fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
+        if (cart.length === 0) {
+          cart = { products: [], totalPrice: 0 };
+        }
       }
       // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
         (prod) => prod.id === id
       );
-
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      // Add new product/increase quantity
-
+      // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
@@ -29,8 +34,7 @@ module.exports = class Cart {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-
-      cart.totalPrice = cart.totalPrice + productPrice;
+      cart.totalPrice = cart.totalPrice + +productPrice;
       fs.writeFile(p, JSON.stringify(cart), (err) => {
         console.log(err);
       });
