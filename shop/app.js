b4f1a67481a877b888f8675150b0d25bda5c5927
chicodevbr <chicodevbr@gmail.com>
2022-02-06 19:02:03 -0300

    conecting db

--- shop/app.js
+++ shop/app.js
@@ -2,12 +2,23 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const errorController = require('./controllers/error');
+
+const db = require('./util/database');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
+
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 app.use(
