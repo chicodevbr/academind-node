892c30d35e66686dda1df8bc6930f467c2bf6f90
chicodevbr <chicodevbr@gmail.com>
2022-02-14 08:24:13 -0300

    connecting with mongodb

--- shop/app.js
+++ shop/app.js
@@ -3,19 +3,13 @@ const express = require('express');
 const app = express();
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-items');
+const mongoConnect = require('./util/database');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+//const adminRoutes = require('./routes/admin');
+//const shopRoutes = require('./routes/shop');
 
 app.use(express.json());
 app.use(
@@ -27,47 +21,24 @@ app.use(
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
-app.use(shopRoutes);
-app.use('/admin', adminRoutes);
+//app.use(shopRoutes);
+//app.use('/admin', adminRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+const PORT = 3000;
 
-sequelize
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: 'art', email: 'artvandelay@email.com' });
-    }
-    return Promise.resolve(user);
-  })
-  .then((user) => {
-    //console.log(user);
-    user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000, () => {
-      console.log('Server Running at port 3000');
-    });
-  })
-  .catch((err) => console.log(err));
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(PORT, () => {
+    console.log(`Server Running at port ${PORT}`);
+  });
+});
