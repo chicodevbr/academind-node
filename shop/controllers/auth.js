f211951f828d9abf857ce90b71e5d90dfde1a598
chicodevbr <chicodevbr@gmail.com>
2022-03-13 19:16:22 -0300

    encrypting password

--- shop/controllers/auth.js
+++ shop/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -38,15 +39,18 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] },
       });
       return user.save();
     })
     .then((result) => {
-      res.redirect('/');
+      res.redirect('/login');
     })
     .catch((err) => {
       console.log(err);
