c6f7b7e12847bb1410d48532c5d62b94c8bf228e
chicodevbr <chicodevbr@gmail.com>
2022-03-14 11:06:05 -0300

    install connect-flash

--- shop/app.js
+++ shop/app.js
@@ -5,6 +5,8 @@ const URL = require('./util/user');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
+
 const app = express();
 
 const errorController = require('./controllers/error');
@@ -44,6 +46,7 @@ app.use(
 );
 
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
