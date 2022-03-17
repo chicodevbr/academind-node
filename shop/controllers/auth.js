ff864116d145ffae85fb48c722d91bee92337261
chicodevbr <chicodevbr@gmail.com>
2022-03-17 11:47:35 -0300

    adding validation and errors message

--- shop/controllers/auth.js
+++ shop/controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const API_KEY = require('../util/mail');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -77,6 +78,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
--- shop/routes/auth.js
+++ shop/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -10,7 +11,16 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Please enter a valid email.'),
+  ],
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
