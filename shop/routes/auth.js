253709e89461a2d6007dd10d4829becba04a41dc
chicodevbr <chicodevbr@gmail.com>
2022-03-18 12:18:20 -0300

    adding login validation

--- shop/routes/auth.js
+++ shop/routes/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,28 +10,7 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post(
-  '/signup',
-  [
-    check('email')
-      .normalizeEmail()
-      .isEmail()
-      .withMessage('Please enter a valid email.'),
-    body(
-      'password',
-      'Please enter a password with only numbers and text and at least 5 characters'
-    )
-      .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body('confirmPassword').custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error('Passwords have to match!');
-      }
-      return true;
-    }),
-  ],
-  authController.postSignup
-);
+router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
