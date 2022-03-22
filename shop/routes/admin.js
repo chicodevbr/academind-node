0fbb1aa37c9e0ee4bd5dffcc785329d4992c55b2
chicodevbr <chicodevbr@gmail.com>
2022-03-22 08:58:47 -0300

    update code

--- shop/routes/admin.js
+++ shop/routes/admin.js
@@ -1,7 +1,10 @@
 const path = require('path');
+
 const express = require('express');
+
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+
 const router = express.Router();
 
 // /admin/add-product => GET
